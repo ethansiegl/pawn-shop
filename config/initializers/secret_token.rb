0c2f474e1f29793986a6f6ed7022fc683716b1d4
Jeff <Dostoyevsky25@gmail.com>
2016-03-02 20:27:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pawnshop::Application.config.secret_key_base = '264bdffbed185157236cc7f012b916647b7a848e7aeb820031aea75e54b47897ce22dbb646afa0f10f898181ee4c67e71d6def9633d558ce109e797ac7d72017'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pawnshop::Application.config.session_store :cookie_store, key: '_pawnshop_session'
