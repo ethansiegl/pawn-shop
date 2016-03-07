3ce8d7703d4bf142f38bca819c773d897d334a92
Zoheb <zoheb_391@hotmail.com>
2016-03-07 05:42:13 +0000

    Edited games model to include two players, a winner and turn

--- db/migrate/20160306052733_create_games.rb
+++ db/migrate/20160306052733_create_games.rb
@@ -1,9 +1,10 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-    	t.integer :player_id
-    	
-
+    	t.integer :white_player_id
+    	t.integer :black_player_id
+    	t.integer :winning_player_id
+    	t.string :turn
       t.timestamps
     end
   end
