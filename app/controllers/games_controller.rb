97a68069ceddbbbd89d3741c0f9f6fd1307ee3c5
LauraUmana <laurau1107@gmail.com>
2016-03-16 05:42:54 +0000

    Added pieces to chess board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,21 @@
 class GamesController < ApplicationController
 
+	def index 
+		@games = Game.all
+	end
+
+	def new
+		@new_game = Game.new
+	end
+
+	def create
+		@game.create(:white_player_id => current_user.id, :black_player_id => nil)
+		redirect_to game_path(@game)
+	end
+
 	def show
 	end 
 
-	def new
-		@game = Game.new
-	end
-
+	def params
+		params.require(:game).permit(:game_id, :white_player_id, :black_player_id)
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
 
 	 def initiate_new_board
 	 	#black pawns
-	 	(0..7).each do |n|
+	 	(1..8).each do |n|
 	 		Pawn.create(
 	 			x_coordinate: n,
 	 			y_coordinate: 6,
@@ -11,32 +11,32 @@ class Game < ActiveRecord::Base
 	 	end
 
 	 	#black pieces
-	 	Rook.create(x_coordinate: 0, y_coordinate: 6, color: 'black')
-	 	Knight.create(x_coordinate: 1, y_coordinate: 6, color: 'black')
-	 	Bishop.create(x_coordinate:2, y_coordinate: 6, color: 'black')
-	 	Queen.create(x_coordinate:3, y_coordinate: 6, color: 'black')
-	 	King.create(x_coordinate:4, y_coordinate: 6, color: 'black')
-	 	Bishop.create(x_coordinate:5, y_coordinate: 6, color: 'black')
-	 	Knight.create(x_coordinate: 6, y_coordinate: 6, color: 'black')
-	 	Rook.create(x_coordinate: 7, y_coordinate: 6, color: 'black')
+	 	Rook.create(x_coordinate: 1, y_coordinate: 8, color: 'black')
+	 	Knight.create(x_coordinate: 2, y_coordinate: 8, color: 'black')
+	 	Bishop.create(x_coordinate: 3, y_coordinate: 8, color: 'black')
+	 	Queen.create(x_coordinate: 4, y_coordinate: 8, color: 'black')
+	 	King.create(x_coordinate: 5, y_coordinate: 8, color: 'black')
+	 	Bishop.create(x_coordinate: 6, y_coordinate: 8, color: 'black')
+	 	Knight.create(x_coordinate: 7, y_coordinate: 8, color: 'black')
+	 	Rook.create(x_coordinate: 8, y_coordinate: 8, color: 'black')
 
 	 	#white pawns
 	 	(0..7).each do |n|
 	 		Pawn.create(
 	 			x_coordinate: n,
-	 			y_coordinate: 0,
+	 			y_coordinate: 2,
 	 			color: 'white')
 	 	end
 
 	 	#white pieces
-	 	Rook.create(x_coordinate: 0, y_coordinate: 0, color: 'white')
-	 	Knight.create(x_coordinate: 1, y_coordinate: 0, color: 'white')
-	 	Bishop.create(x_coordinate:2, y_coordinate: 0, color: 'white')
-	 	Queen.create(x_coordinate:3, y_coordinate: 0, color: 'white')
-	 	King.create(x_coordinate:4, y_coordinate: 0, color: 'white')
-	 	Bishop.create(x_coordinate:5, y_coordinate: 0, color: 'white')
-	 	Knight.create(x_coordinate: 6, y_coordinate: 0, color: 'white')
-	 	Rook.create(x_coordinate: 7, y_coordinate: 0, color: 'white')
+	 	Rook.create(x_coordinate: 1, y_coordinate: 1, color: 'white')
+	 	Knight.create(x_coordinate: 2, y_coordinate: 1, color: 'white')
+	 	Bishop.create(x_coordinate: 3, y_coordinate: 1, color: 'white')
+	 	Queen.create(x_coordinate: 4, y_coordinate: 1, color: 'white')
+	 	King.create(x_coordinate: 5, y_coordinate: 1, color: 'white')
+	 	Bishop.create(x_coordinate: 6, y_coordinate: 1, color: 'white')
+	 	Knight.create(x_coordinate: 7, y_coordinate: 1, color: 'white')
+	 	Rook.create(x_coordinate: 8, y_coordinate: 1, color: 'white')
 
 	 end
 end
