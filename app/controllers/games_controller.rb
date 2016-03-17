3583c9b5d8b1c8570327dcf7fa9a163a6f5f8808
Jeff <Dostoyevsky25@gmail.com>
2016-03-17 04:37:35 +0000

    uncomment show method in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+	def index
+		@games = Game.all
+	end
+
+	def new
+		@new_game = Game.new
+	end
+
+	def create
+		@game.create(:white_player_id => current_user.id, :black_player_id => nil)
+		redirect_to game_path(@game)
+	end
+
+	def show
+		@game = Game.find(params[:id])
+	end
+
+	def params
+		params.require(:game).permit(:game_id, :white_player_id, :black_player_id)
+	end
+
 end
