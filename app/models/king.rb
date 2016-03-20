97271733ec32538d194e4061745f7cc5b6be19dd
Jeff <Dostoyevsky25@gmail.com>
2016-03-20 16:28:12 +0000

    started valid move checks for king piece

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,14 +1,13 @@
 class King < Piece
 
-	def valid_move?
-
-		#if moves one space 
-			#return true
-
-		#else
-			#return false
-		#end
-
+	def valid_move?(x_coor, y_coor)
+		start_pos_x = start_x
+		start_pos_y = start_y
+		if w == x + 1 || w == x - 1 || z == y + 1 || z == y - 1
+			return true
+		else
+				return false
+		end
 	end
 
 end
