eb0e0238817129226a550796cf8fd4a6ec269eeb
Jeff <Dostoyevsky25@gmail.com>
2016-03-21 22:17:50 +0000

    added passing rspec tests

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,10 @@
 class King < Piece
 
 	def valid_move?(move_x, move_y)
-
-		if abs(move_x - x_coordinate) <= 1 && abs(move_y - y_coordinate) <= 1 && (move_x =! x_coordinate || move_y =! y_coordinate)
+		if (move_x - x_coordinate).abs == 1 || (move_y - y_coordinate).abs == 1
 			return true
 		else
-				return false
+			return false
 		end
 	end
 
--- spec/models/king_test.rb
+++ spec/models/king_test.rb
@@ -3,23 +3,32 @@ require 'rails_helper'
 RSpec.describe King, type: :model do
   before :each do
     @game = Game.new
+    @white_king = King.create(
+      x_coordinate: 2,
+      y_coordinate: 2,
+      game: @game,
+      color: "white"
+    )
   end
 
   describe "valid move for King piece" do
     it "should return true if King moves one square horizontially" do
+      expect(@white_king.valid_move?(2, 1)).to eq true
+    end
 
+    it "should return true is King moves one square diagonally" do
+      expect(@white_king.valid_move?(3,3)).to eq true
     end
 
     it "should return false if King moves more than one square" do
-
+      expect(@white_king.valid_move?(6,7)).to eq false
     end
 
     it "should return false if King stays in one place" do
-
+      expect(@white_king.valid_move?(2,2)).to eq false
     end
 
   end
-
 
 
 end
