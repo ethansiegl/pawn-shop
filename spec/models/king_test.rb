cd7bf1bf0d4fdf2839419c50dc7b8f354457fc31
Jeff <Dostoyevsky25@gmail.com>
2016-03-21 16:19:43 +0000

    continuing rspec tests for king move

--- spec/models/king_test.rb
+++ spec/models/king_test.rb
@@ -1,9 +1,25 @@
 require rails_helper
 
 Rspec.describe King, type => :model do
-@game = Game.new
+  before :each do
+    @game = Game.new
+  end
 
   describe "valid move for King piece" do
+    it "should return true if King moves one square horizontially" do
 
+    end
+
+    it "should return false if King moves more than one square" do
+
+    end
+
+    it "should return false if King stays in one place" do
+
+    end
+      
   end
+
+
+
 end
