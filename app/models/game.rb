a6bdaa7a4f7762a7e423f1d9f225f6234d6b1859
Zoheb <zoheb_391@hotmail.com>
2016-03-23 05:26:41 +0000

    black_player can access the game/needs work to update black_player_id

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,18 +12,18 @@ class Game < ActiveRecord::Base
 	 			x_coordinate: n,
 	 			y_coordinate: 6,
 	 			color: 'black',
-	 			game: self)
+	 			game_id: id)
 	 	end
 
 	 	#black pieces
-	 	Rook.create(x_coordinate: 1, y_coordinate: 8, color: 'black', game: self)
-	 	Knight.create(x_coordinate: 2, y_coordinate: 8, color: 'black', game: self)
-	 	Bishop.create(x_coordinate: 3, y_coordinate: 8, color: 'black', game: self)
-	 	Queen.create(x_coordinate: 4, y_coordinate: 8, color: 'black', game: self)
-	 	King.create(x_coordinate: 5, y_coordinate: 8, color: 'black', game: self)
-	 	Bishop.create(x_coordinate: 6, y_coordinate: 8, color: 'black', game: self)
-	 	Knight.create(x_coordinate: 7, y_coordinate: 8, color: 'black', game: self)
-	 	Rook.create(x_coordinate: 8, y_coordinate: 8, color: 'black', game: self)
+	 	Rook.create(x_coordinate: 1, y_coordinate: 8, color: 'black', game_id: id)
+	 	Knight.create(x_coordinate: 2, y_coordinate: 8, color: 'black', game_id: id)
+	 	Bishop.create(x_coordinate: 3, y_coordinate: 8, color: 'black', game_id: id)
+	 	Queen.create(x_coordinate: 4, y_coordinate: 8, color: 'black', game_id: id)
+	 	King.create(x_coordinate: 5, y_coordinate: 8, color: 'black', game_id: id)
+	 	Bishop.create(x_coordinate: 6, y_coordinate: 8, color: 'black', game_id: id)
+	 	Knight.create(x_coordinate: 7, y_coordinate: 8, color: 'black', game_id: id)
+	 	Rook.create(x_coordinate: 8, y_coordinate: 8, color: 'black', game_id: id)
 
 	 	#white pawns
 	 	(0..7).each do |n|
@@ -31,19 +31,19 @@ class Game < ActiveRecord::Base
 	 			x_coordinate: n,
 	 			y_coordinate: 2,
 	 			color: 'white',
-	 	 		game: self)
+	 	 		game_id: id)
 	 			
 	 	end
 
 	 	#white pieces
-	 	Rook.create(x_coordinate: 1, y_coordinate: 1, color: 'white', game: self)
-	 	Knight.create(x_coordinate: 2, y_coordinate: 1, color: 'white', game: self)
-	 	Bishop.create(x_coordinate: 3, y_coordinate: 1, color: 'white', game: self)
-	 	Queen.create(x_coordinate: 4, y_coordinate: 1, color: 'white', game: self)
-	 	King.create(x_coordinate: 5, y_coordinate: 1, color: 'white', game: self)
-	 	Bishop.create(x_coordinate: 6, y_coordinate: 1, color: 'white', game: self)
-	 	Knight.create(x_coordinate: 7, y_coordinate: 1, color: 'white', game: self)
-	 	Rook.create(x_coordinate: 8, y_coordinate: 1, color: 'white', game: self)
+	 	Rook.create(x_coordinate: 1, y_coordinate: 1, color: 'white', game_id: id)
+	 	Knight.create(x_coordinate: 2, y_coordinate: 1, color: 'white', game_id: id)
+	 	Bishop.create(x_coordinate: 3, y_coordinate: 1, color: 'white', game_id: id)
+	 	Queen.create(x_coordinate: 4, y_coordinate: 1, color: 'white', game_id: id)
+	 	King.create(x_coordinate: 5, y_coordinate: 1, color: 'white', game_id: id)
+	 	Bishop.create(x_coordinate: 6, y_coordinate: 1, color: 'white', game_id: id)
+	 	Knight.create(x_coordinate: 7, y_coordinate: 1, color: 'white', game_id: id)
+	 	Rook.create(x_coordinate: 8, y_coordinate: 1, color: 'white', game_id: id)
 
 	 end
 end
