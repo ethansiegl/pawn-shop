5b5e76d949c9f8244edf62dc2bbe90e87bfbe8f5
Ethan <09esiegel@gmail.com>
2016-03-25 20:51:32 +0000

    refactor

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,27 +1,12 @@
 class Rook < Piece
 	
 	def valid_move?(destination_x, destination_y)
-		# return false if !move_to?(destination_x, destination_y)
-
-		
-		return false if self.is_obstructed?(destination_x, destination_y)
-		
-		destination_square = game.pieces.where(x_coordinate: destination_x, y_coordinate: destination_y).take
+		return false if is_obstructed?(destination_x, destination_y)
 
 		if x_coordinate != destination_x && y_coordinate != destination_y
 			return false
 		end
-
-		
-
 		true
 	end
+	
 end
-
-# query origin square location (x,y)
-
-# find destination square location (x,y)
-
-# determine whether destination has same x or y
-
-# return true or false depending on x and y values 
