212a4bde5700f0c72e130767e3c0a1e627dd38d1
Jeff <Dostoyevsky25@gmail.com>
2016-03-27 18:26:41 +0000

    added rspec tests for queen valid move

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -3,7 +3,7 @@ class Queen < Piece
 	def valid_move?(move_x, move_y)
 		if ((move_x + x_coordinate).abs <= 8) &&
 			((move_y + y_coordinate).abs <= 8) &&
-			((move_x != x_coordinate) && (move_y != y_coordinate))
+			((move_x != x_coordinate) || (move_y != y_coordinate))
 			return true
 		else
 			return false
--- spec/models/queen_test.rb
+++ spec/models/queen_test.rb
@@ -12,8 +12,16 @@ RSpec.describe Queen, type: :model do
   end
 
   describe "should return true if queen move is valid" do
-    it "should return true if the queen move is valid" do
+    it "should return true if the queen moves vertically" do
       expect(@white_queen.valid_move?(5, 7)).to eq true
+    end
+
+    it "should return false if the queen move is invalid" do
+      expect(@white_queen.valid_move?(0,1)).to eq false
+    end
+
+    it "should return false if the queen stays in the same place" do
+      expect(@white_queen.valid_move?(5, 7)).to eq false
     end
   end
 end
