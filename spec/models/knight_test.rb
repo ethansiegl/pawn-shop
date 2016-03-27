3e963f046ced4eef8c982ff6e6aa85efa1a5df71
Jeff <Dostoyevsky25@gmail.com>
2016-03-27 15:53:03 +0000

    added tests for knight moves

--- spec/models/knight_test.rb
+++ spec/models/knight_test.rb
@@ -15,6 +15,18 @@ RSpec.describe Knight, type: :model do
     it "should return true if the knight makes valid move" do
       expect(@white_knight.valid_move?(6, 5)).to eq true
     end
+
+    it "should return true if the knight makes a different valid move" do
+      expect(@white_knight.valid_move?(3, 6)).to eq true
+    end
+
+    it "should return false if the knight makes an invalid move" do
+      expect(@white_knight.valid_move?(3,3)).to eq false
+    end
+
+    it "should return false if the knight stays in one place" do
+      expect(@white_knight.valid_move?(4,4)).to eq false
+    end
   end
 
 end
