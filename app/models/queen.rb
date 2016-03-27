6f8e5b422376eb399d3610dd96b511dbf7367393
Jeff <Dostoyevsky25@gmail.com>
2016-03-27 04:25:36 +0000

    added knight valid move method and started rspec tests

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,7 +1,11 @@
 class Queen < Piece
 
 	def valid_move?(move_x, move_y)
-		if ## horizontal / vertical /diagonal
+		if #horizontal
+			#vertical
+			#diagonal_left
+			#diagonal_right
+			&& ((move_x != x_coordinate) && (move_y != y_coordinate))
 			return true
 		else
 			return false
