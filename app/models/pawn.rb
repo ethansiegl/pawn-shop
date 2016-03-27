4c8c7e5162ddc865315687784b6b302344ed09b6
Zoheb <zoheb_391@hotmail.com>
2016-03-27 07:34:16 +0000

    pawn valid move/needs work

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -3,6 +3,7 @@ class Pawn < Piece
 	def valid_move?(x,y)
 		return false if is_obstructed?(x,y)
 		return false if !on_board?(x,y)
+		return false if backward_move(x,y)
 		if !taking_opponent?
 			return false if x != x_coordinate
 		end
@@ -20,6 +21,11 @@ class Pawn < Piece
 	
 	def taking_opponent?(x,y)
 		return false if piece_at(x,y) == friendly_piece?
+	end
+
+	def backward_move?(x,y)
+		return true if (is_white? && y<y_coordinate) && (!is_white? && y>y_coordinate)
+		else return false
 	end
 
 
