5ce4f9f8b371d72e52e39804b359d22c8b87a1a5
Jeff <Dostoyevsky25@gmail.com>
2016-03-29 03:13:43 +0000

    added an additional test to knight moves to test on_board method

--- spec/models/knight_test.rb
+++ spec/models/knight_test.rb
@@ -4,28 +4,32 @@ RSpec.describe Knight, type: :model do
   before :each do
     @game = Game.new
     @white_knight = Knight.create(
-      x_coordinate: 4,
-      y_coordinate: 4,
+      x_coordinate: 1,
+      y_coordinate: 2,
       game: @game,
       color: "white"
     )
   end
 
   describe "valid move for knight" do
-    it "should return true if the knight makes valid move" do
-      expect(@white_knight.valid_move?(6, 5)).to eq true
+    it "should return true if the knight moves two horizontal spaces and one vertical space" do
+      expect(@white_knight.valid_move?(3, 3)).to eq true
     end
 
-    it "should return true if the knight makes a different valid move" do
-      expect(@white_knight.valid_move?(3, 6)).to eq true
+    it "should return true if the knight moves one horizontal space and two vertical spaces" do
+      expect(@white_knight.valid_move?(2, 4)).to eq true
     end
 
     it "should return false if the knight makes an invalid move" do
-      expect(@white_knight.valid_move?(3,3)).to eq false
+      expect(@white_knight.valid_move?(2, 3)).to eq false
     end
 
     it "should return false if the knight stays in one place" do
-      expect(@white_knight.valid_move?(4,4)).to eq false
+      expect(@white_knight.valid_move?(1, 2)).to eq false
+    end
+
+    it "should return false if the knight moves off board" do
+      expect(@white_knight.valid_move?(8,9)).to eq false
     end
   end
 
