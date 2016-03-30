729068087b5f535d80eea2f574ade76f1c07635b
Ethan <09esiegel@gmail.com>
2016-03-30 01:03:43 +0000

    fixed on_board method. is_obstructed still breaks valid_move? method

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,13 +1,9 @@
 class Bishop < Piece
 
 	def valid_move?(x, y)
+		# return false if is_obstructed?(x, y) # this method breaks the function for some reason
+		return false if !on_board?(x,y) # fixed helper method
 		(x - x_coordinate).abs == (y - y_coordinate).abs
-		# return false if is_obstructed?(x, y)
-		#return false if !on_board?(x, y)
-
-
-
-		
 	end
 
 end
