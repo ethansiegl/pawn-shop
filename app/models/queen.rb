9c02c40d45a378463350967e64814cfeecbccf05
Jeff <Dostoyevsky25@gmail.com>
2016-04-01 13:00:05 +0000

    cleaned up queen model

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,6 +1,5 @@
 class Queen < Piece
 
-<<<<<<< HEAD
 	def valid_move?(x,y)
 		return false if !on_board?(x,y)
 		return false if no_move?(x,y)
@@ -11,18 +10,5 @@ class Queen < Piece
 			return false
 		end
 	end
-=======
-	#def valid_move?(move_x, move_y)
-		#if horizontal
-			#vertical
-			#diagonal_left
-			#diagonal_right
-			##&& ((move_x != x_coordinate) && (move_y != y_coordinate))
-			#return true
-		#else
-			#return false
-		#end
-	#end
 
->>>>>>> 168dfc9aa484faaeb11b56235fe3a9fd13f4d906
 end
