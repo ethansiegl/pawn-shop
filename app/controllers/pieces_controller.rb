102bdde449116187c91c81797f257b5d14e52bf0
Jeff <Dostoyevsky25@gmail.com>
2016-04-05 14:55:51 +0000

    unmerged is_check and master

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,7 +1,7 @@
 class PiecesController < ApplicationController
 
 	def show
-		@piece = Game.Piece.find(params[:id])
+		@piece = Piece.find(params[:id])
 	end
 
 	def update
@@ -13,16 +13,17 @@ class PiecesController < ApplicationController
 
 	helper_method :current_game, :current_piece
 
+
+	def piece_params
+		params.require(:piece).permit(:x_coordinate, :y_coordinate)
+	end
+
 	def current_game
 		@current_game ||= Game.find(params[:game_id])
 	end
 
 	def current_piece
 		@piece ||= Piece.find(params[:id])
-	end
-
-	def piece_params
-		params.require(:piece).permit(:x_coordinate, :y_coordinate)
 	end
 
 end
