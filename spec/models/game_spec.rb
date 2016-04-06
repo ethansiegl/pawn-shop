004db5957dff613b3050f9a11419c246e120f507
LauraUmana <laurau1107@gmail.com>
2016-04-06 04:27:33 +0000

    Made some changes to test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -18,14 +18,14 @@ RSpec.describe Game, type: :model do
 				y_coordinate: 2,
 				game: @game,
 				color: "white",
-				turn: white_player_id)
+				)
 
 			@black_rook = Rook.create(
 				x_coordinate: 2,
 				y_coordinate: 4,
 				game: @game,
 				color: "white",
-				turn: white_player_id)
+				)
 			expect(@black_rook.valid_move?(2, 2)).to eq true
 
 		end
