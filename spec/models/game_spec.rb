4cf623bbbb78d1cba0a13d97e1b0218cfaf2caf7
LauraUmana <laurau1107@gmail.com>
2016-04-06 04:26:36 +0000

    Created is_check test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,5 +8,27 @@ RSpec.describe Game, type: :model do
 			expect(g.pieces.count).to eq 32
 		end
 	end
+
+	describe "checking the status of game if in check" do
+
+		it "should return true if the game is in check" do
+			@game = Game.new
+			@white_king = King.create(
+				x_coordinate: 2,
+				y_coordinate: 2,
+				game: @game,
+				color: "white",
+				turn: white_player_id)
+
+			@black_rook = Rook.create(
+				x_coordinate: 2,
+				y_coordinate: 4,
+				game: @game,
+				color: "white",
+				turn: white_player_id)
+			expect(@black_rook.valid_move?(2, 2)).to eq true
+
+		end
+	end
 end
 
