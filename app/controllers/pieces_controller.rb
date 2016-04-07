c23227f90bfb2ed8356a6fa50be3bdcebbe2f98e
Ethan <09esiegel@gmail.com>
2016-04-07 15:26:00 +0000

    updated piece controller from is_check branch

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,18 +1,22 @@
 class PiecesController < ApplicationController
-
 	def show
 		@piece = Piece.find(params[:id])
 	end
-
+	
 	def update
-		current_piece.update(:x_coordinate => params[:row].to_i, :y_coordinate => params[:column].to_i)
-		render text: 'Opponent move'
-	end
+		if current_piece.valid_move?(params[:row].to_i, params[:column].to_i)
+			current_piece.move_to!(params[:row], params[:column])
+			redirect_to game_path(current_game)
+		else
+			redirect_to game_path(current_game)
+			flash[:alert] = "Invalid Move"
+		end
+	end	
 
 	private
 
-	helper_method :current_game, :current_piece
-
+	helper_method :current_game, :current_piece	
+	
 	def piece_params
 		params.require(:piece).permit(:x_coordinate, :y_coordinate)
 	end
@@ -20,7 +24,7 @@ class PiecesController < ApplicationController
 	def current_game
 		@current_game ||= Game.find(params[:game_id])
 	end
-
+	
 	def current_piece
 		@piece ||= Piece.find(params[:id])
 	end
