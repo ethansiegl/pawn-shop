f5f61ba1995de2bd0e9fcadf579c8ae5b17b63ed
Ethan <09esiegel@gmail.com>
2016-04-19 15:58:56 +0000

    pawn model first_move undo

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -25,9 +25,11 @@ class Pawn < Piece
 	end
 
 	def first_move?
-		return true if color == 'white' && position_y == 2
-    return true if color == 'black' && position_y == 7
-    false
+		if (y_coordinate == 2 && is_white?(self)) || (y_coordinate == 7 && !is_white?(self))
+			return true
+		else
+			return false
+		end
 	end
 
 	def backward_move?(x,y)
